888703cd1ebf66d0de7989307897b52b7443f018
Shmuel <s600hho@gmail.com>
2022-06-15 21:09:09 +0300

    products are now cards on main page

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { useContext, useState, useRef } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import CartContext from "../../contexts/CartContext";
 import './Product.css';
@@ -28,37 +28,59 @@ const Product = ({ product }) => {
         setRenderToggle(!renderToggle);
     }
 
-    const focusedCard = useRef(null);
-    const unfocus = () => focusedCard.blur;
     return (
         <>
-            {/* < Link to={`/products${product.id}`} onClick={() => setLoadFilters(false)}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
-                        <CardMedia className="product-image"
+            < Link
+                to={`/products${product.id}`}
+                className="disable-link"
+                onClick={() => setLoadFilters(false)}
+                onMouseOut={(e) => e.target.blur()}
+            >
+                <Card sx={{ margin: 0.5, minWidth: 260, maxWidth: 260, minHeight: 370, maxHeight: 370, borderRadius: 10 }} className="card">
+                    <div className="content">
+                        <div className="description">More Details</div>
+                        <div className="btn">
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                remove();
+                            }}>↓</button>
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                add();
+                            }}>↑</button>
+                            <input value={howMany} readOnly />
+                            <button onClick={(e) => {
+                                e.preventDefault();
+                                addAll();
+                            }} >
+                                Add To Cart
+                            </button>
+                        </div>
+                    </div>
+                    <CardActionArea className="product-image">
+                        <CardMedia className="media"
                             component="img"
                             height="140"
                             image={product.image}
                             alt="image not found"
                         />
-                        <CardContent className="product-info">
-                            <Typography gutterBottom variant="h5" component="div">
+                        <CardContent className="product-info card-content">
+                            <Typography gutterBottom variant="h5" component="div" className='title'>
                                 {product.title}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
+                            <Typography variant="body2" color="text.secondary" className='price'>
                                 {product.price}
                             </Typography>
                         </CardContent>
                     </CardActionArea>
                 </Card>
-            </Link> */}
+            </Link>
 
-            < Link
+            {/* < Link
                 to={`/products${product.id}`}
-                className="product-card card"
+                className="product-card card card-size"
                 onClick={() => setLoadFilters(false)}
                 onMouseOut={(e) => e.target.blur()}
-                ref={focusedCard}
             >
                 <div className="product-image">
                     <img src={product.image} alt="" />
@@ -88,7 +110,7 @@ const Product = ({ product }) => {
                     <h6>${product.price}</h6>
                     <br />
                 </div>
-            </Link >
+            </Link > */}
         </>
     );
 }
